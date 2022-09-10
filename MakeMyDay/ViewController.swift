7b5c3040f0319afd7974c5afb9bae7134e2daa5d
yyyng2 <yyyng2@gmail.com>
2022-09-10 20:25:12 +0900

    Initial Commit

---
+++ MakeMyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeMyDay
+//
+//  Created by Y on 2022/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
